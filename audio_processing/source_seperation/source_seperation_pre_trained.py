5e78934ff5189f147a9ba9c94ca40d7313398d6e
khalil8500 <khalil8500@gmail.com>
2022-07-13 11:25:11 -0500

    add source separation folders

--- audio_processing/source_seperation/source_seperation_pre_trained.py
+++ audio_processing/source_seperation/source_seperation_pre_trained.py
@@ -5,8 +5,8 @@ import torchaudio
 file_path = 'add wav file.wav'
 model = separator.from_hparams(source="speechbrain/sepformer-wsj03mix", savedir='pretrained_models/sepformer-wsj03mix')
 
-est_sources = model.separate_file(path=file_path) 
+#est_sources = model.separate_file(path=file_path) 
 
-torchaudio.save("source1hat.wav", est_sources[:, :, 0].detach().cpu(), 8000)
-torchaudio.save("source2hat.wav", est_sources[:, :, 1].detach().cpu(), 8000)
-torchaudio.save("source3hat.wav", est_sources[:, :, 2].detach().cpu(), 8000)
+#torchaudio.save("source1hat.wav", est_sources[:, :, 0].detach().cpu(), 8000)
+#torchaudio.save("source2hat.wav", est_sources[:, :, 1].detach().cpu(), 8000)
+#torchaudio.save("source3hat.wav", est_sources[:, :, 2].detach().cpu(), 8000)
