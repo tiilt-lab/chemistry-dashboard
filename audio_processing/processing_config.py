ba7a7f6ee63c018e1dc5cf88a8817845318268a5
Tochukwu Eze <domisky2003@yahoo.com>
2022-08-18 00:31:13 -0500

    video transfer and saving

--- audio_processing/processing_config.py
+++ audio_processing/processing_config.py
@@ -71,3 +71,78 @@ class ProcessingConfig:
 
     def is_valid_key(self):
         return RedisSessions.get_device_key(self.auth_key) != None
+
+class ProcessingVideoConfig:
+    def __init__(self, auth_key, session_key, server_start, start_offset, audio_rate,video_rate, encoding, channels, transcribe, keywords, doa, features, tag, diarization):
+        self.auth_key = auth_key
+        self.session_key = session_key
+        self.server_start = server_start
+        self.start_offset = start_offset
+        self.audio_rate = audio_rate
+        self.video_rate = video_rate
+        self.encoding = encoding
+        self.depth = 2 #if self.encoding in ['pcm_f16le', 'pcm_i16le'] else 4
+        self.channels = channels
+        self.transcribe = transcribe
+        self.keywords = keywords
+        self.doa = doa
+        self.features = features
+        self.tag = tag
+        self.diarization = True # Default to true for testing.
+
+    @staticmethod
+    def from_json(data):
+        auth_key = data.get('key', None)
+        encoding = data.get('encoding', None)
+        try:
+            audio_rate = int(data.get('audio_rate', None))
+        except Exception as e:
+            return False, "audio_rate must be an integer."
+        try:
+            video_rate = int(data.get('video_rate', None))
+        except Exception as e:
+            return False, "video_rate must be an integer."    
+        try:
+            channels = int(data.get('channels', None))
+        except Exception as e:
+            return False, "channels must be an integer."
+        try:
+            offset = float(data.get('offset', 0.0))
+        except Exception as e:
+            return False, "offset must be a float."
+
+        # Check if fields are missing.
+        if not auth_key or not audio_rate or not video_rate or not encoding or not channels:
+            return False, "Start messege requires key, audio_rate, video_rate, encoding, and channels."
+        # Check if format is supported.
+        if not encoding in ['video/mp4', 'video/webm']:
+            return False, "Unsupported encoding type."
+        # Check if sample rate is supported.
+        if not audio_rate in [128000]:
+            return False, "Unsupported audio bit rate."
+        if not video_rate in [2500000]:
+            return False, "Unsupported video bit rate."    
+        # Check if tagging is requested and is possible.
+        tag = data.get('tag', False) and cf.record_original() and channels == 2
+        # Check if diarization is requested and possible.
+        diarization = data.get('tag', False) and channels == 6
+
+        # Check if auth is required and if key is valid.
+        session_key = RedisSessions.get_device_key(auth_key)
+        if session_key:
+            RedisSessions.get_session_config(session_key)
+            session_config = json.loads(RedisSessions.get_session_config(session_key))
+            server_start = datetime.strptime(session_config.get('server_start', None), "%Y-%m-%d %H:%M:%S")
+            start_offset = max((datetime.utcnow() - server_start).total_seconds() - offset, 0.0)
+            transcribe = session_config.get('transcribe', True)
+            features = session_config.get('features', False)
+            keywords = session_config.get('keywords', [])
+            doa = session_config.get('doa', False)
+        else:
+            logging.warning('Invalid key sent by device.')
+            return False, "Invalid key."
+
+        return True, ProcessingVideoConfig(auth_key, session_key, server_start, start_offset, audio_rate, video_rate, encoding, channels, transcribe, keywords, doa, features, tag, diarization)
+
+    def is_valid_key(self):
+        return RedisSessions.get_device_key(self.auth_key) != None        
--- audio_processing/recorder.py
+++ audio_processing/recorder.py
@@ -1,3 +1,4 @@
+from genericpath import isfile
 import numpy as np
 from scipy.io import wavfile
 import os
@@ -27,5 +28,33 @@ class WaveRecorder:
                 chunk_length = int(len(all_data) / self.channels)
                 all_data = np.reshape(all_data, (chunk_length, self.channels))
             wavfile.write(self.wav_filename, self.sample_rate, all_data)
+            os.remove(self.dat_filename)
+            self.closed = True
+
+class VidRecorder:
+
+    def __init__(self, filename):
+        self.dat_filename = filename + '.dat'
+        self.vid_filename = filename + '.mp4'
+        self.closed = False
+
+    def write(self, data):
+        try:
+            with open(self.dat_filename, "ab") as f:
+                f.write(data)
+        except Exception as e:
+            print('Unable to write data to file: {0}'.format(e))
+
+    def close(self):
+        if not self.closed:
+            with open(self.dat_filename, "rb") as f:
+                all_data = f.read()
+            
+            if os.path.isfile(self.vid_filename):
+                os.remove(self.vid_filename)
+
+            with open(self.vid_filename, "wb") as out_file:
+                out_file.write(all_data)  
+                  
             os.remove(self.dat_filename)
             self.closed = True
