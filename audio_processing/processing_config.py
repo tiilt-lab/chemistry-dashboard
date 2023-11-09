3f9e5144551787a93b4fdffd86a304869fedb49e
khalil8500 <khalil8500@gmail.com>
2023-11-09 10:23:26 -0600

    Added topic_model field to table and processing with topic models, need to ensure accuracy next

--- audio_processing/processing_config.py
+++ audio_processing/processing_config.py
@@ -5,7 +5,7 @@ import logging
 import config as cf
 
 class ProcessingConfig:
-    def __init__(self, auth_key, session_key, server_start, start_offset, sample_rate, encoding, channels, transcribe, keywords, doa, features, tag, diarization, embeddingsFile):
+    def __init__(self, auth_key, session_key, server_start, start_offset, sample_rate, encoding, channels, transcribe, keywords, doa, features, tag, diarization, embeddingsFile, topic_model, owner):
         self.auth_key = auth_key
         self.session_key = session_key
         self.server_start = server_start
@@ -21,6 +21,8 @@ class ProcessingConfig:
         self.tag = tag
         self.diarization = True # Default to true for testing.
         self.embeddingsFile = embeddingsFile
+        self.topic_model = topic_model
+        self.owner = owner
 
     @staticmethod
     def from_json(data):
@@ -65,11 +67,14 @@ class ProcessingConfig:
             features = session_config.get('features', False)
             keywords = session_config.get('keywords', [])
             doa = session_config.get('doa', False)
+            topic_model = session_config.get('topic_model', None)
+            owner = session_config.get('owner', None)
+
         else:
             logging.warning('Invalid key sent by device.')
             return False, "Invalid key."
 
-        return True, ProcessingConfig(auth_key, session_key, server_start, start_offset, sample_rate, encoding, channels, transcribe, keywords, doa, features, tag, diarization, embeddingsFile)
+        return True, ProcessingConfig(auth_key, session_key, server_start, start_offset, sample_rate, encoding, channels, transcribe, keywords, doa, features, tag, diarization, embeddingsFile, topic_model, owner)
 
     def is_valid_key(self):
         return RedisSessions.get_device_key(self.auth_key) != None
