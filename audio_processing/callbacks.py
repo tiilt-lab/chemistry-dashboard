4375bf9c0f2c8bcf65ef9d90f7f2982bf983dc1e
khalil8500 <khalil8500@gmail.com>
2024-04-12 11:37:26 -0500

    modified config to work with main server

--- audio_processing/callbacks.py
+++ audio_processing/callbacks.py
@@ -3,6 +3,7 @@ import requests
 import os
 from datetime import datetime
 import logging
+import base64
 
 def post_transcripts(source, start_time, end_time, transcript, doa, questions, keywords, features):
     result = {
@@ -44,9 +45,13 @@ def post_connect(source):
         'time': str(datetime.utcnow())
     }
     try:
+        logging.info('end point')
+        logging.info(config.connect_callback())
         response = requests.post(config.connect_callback(), json=connection)
+        logging.info(response.status_code)
         return response.status_code == 200
     except Exception as e:
+        logging.info('connect callback failed: {0}'.format(e))
         return False
 
 def post_disconnect(source):
---
+++ video_processing/callbacks.py
@@ -0,0 +1,55 @@
+import config
+import requests
+import os
+from datetime import datetime
+import logging
+import base64
+
+    
+def post_cartoonized_image(source, sessionId,deviceId, byteimage):
+    encoded = base64.b64encode(byteimage) 
+    result = {
+        'source': source,
+        'sessionid' : sessionId,
+        'deviceid' : deviceId,
+        'image': encoded
+    }
+    try:
+        response = requests.post(config.cartoonize_image_callback(),json=result)
+        return response.status_code == 200
+    except Exception as e:
+        logging.info('cartoonize image callback  failed: {0}'.format(e))
+        return False
+
+def get_redis_session_key(auth_key):
+    payload = {
+        'auth_key': auth_key
+    }
+    try:
+        response = requests.post(config.redis_session_key_callback(),json=payload)
+        if response.status_code == 200:
+            data = response.json()
+            return data['redis_key']
+        else:
+            logging.info('get_redis_device_key  failed: {0}'.format(response))  
+            return None 
+    except Exception as e:
+        logging.info('get_redis_device_key  failed: {0}'.format(e))
+        return None        
+ 
+
+def get_redis_session_config(session_key):
+    payload = {
+    'session_key': session_key
+    }
+    try:
+        response = requests.post(config.redis_session_config_callback(),json=payload)
+        if response.status_code == 200:
+            data = response.json()
+            return data['redis_session_key']
+        else:
+            logging.info('get_redis_session_config callback  failed: {0}'.format(response))  
+            return None 
+    except Exception as e:
+        logging.info('get_redis_session_config callback  failed: {0}'.format(e))
+        return None  
