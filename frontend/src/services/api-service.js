9e6582cee41ed31cc467a8f59624626c7c6a7d4c
Tochukwu Eze <domisky2003@yahoo.com>
2024-05-16 12:54:05 +0000

    pushing to emotion detection branch

--- frontend/src/services/api-service.js
+++ frontend/src/services/api-service.js
@@ -1,90 +1,72 @@
-
 //import { Observable } from 'rxjs';
 export class ApiService {
-  getWSSProtocol() {
-    return window.location.protocol === "https:" ? "wss:" : "ws:";
-  }
-
-  getEndpoint() {
-    return (
-      window.location.protocol + "//" + window.location.host + "/"
-    );
-  }
-
-  getVideoServerEndpoint() {
-    // return (
-    //   "http://129.105.44.121:8080/"
-    // );
     getWSSProtocol() {
-      return window.location.protocol === "https:" ? "wss:" : "ws:";
+        return window.location.protocol === "https:" ? "wss:" : "ws:"
     }
-  
+
     getEndpoint() {
-      return (
-        window.location.protocol + "//" + window.location.host + "/"
-      );
+        return window.location.protocol + "//" + window.location.host + "/"
     }
-  
+
+    getWSSProtocol() {
+        return window.location.protocol === "https:" ? "wss:" : "ws:"
+    }
+
+    getEndpoint() {
+        return window.location.protocol + "//" + window.location.host + "/"
+    }
+
     getVideoServerEndpoint() {
-    //   return (
-    //     "http://129.105.44.121:8080/"
-    //   );
-    return (
-      "https://video.tiilt-blinc.com"
-    );
-    
-  }
-
-  getAudioWebsocketEndpoint() {
-    return (
-      this.getWSSProtocol() +
-      "//" +
-      // window.location.host.split(":")[0] +
-      window.location.host +
-      "/audio_socket"
-    );
-  }
-
-  getVideoWebsocketEndpoint() {
-    // return (
-    //   "ws://129.105.44.121:8080/video_socket"
-    // );
-
-        window.location.protocol + "//" + window.location.host + "/"
-      );
+        //   return (
+        //     "http://129.105.44.121:8080/"
+        //   );
+        return "https://video.tiilt-blinc.com"
     }
-  
+
     getAudioWebsocketEndpoint() {
-      return (
-        this.getWSSProtocol() +
-        "//" +
-        // window.location.host.split(":")[0] +
-        window.location.host +
-        "/audio_socket"
-      );
+        return (
+            this.getWSSProtocol() +
+            "//" +
+            // window.location.host.split(":")[0] +
+            window.location.host +
+            "/audio_socket"
+        )
     }
-  
+
+    getVideoWebsocketEndpoint() {
+        // return (
+        //   "ws://129.105.44.121:8080/video_socket"
+        // );
+        return window.location.protocol + "//" + window.location.host + "/"
+    }
+
+    getAudioWebsocketEndpoint() {
+        return (
+            this.getWSSProtocol() +
+            "//" +
+            // window.location.host.split(":")[0] +
+            window.location.host +
+            "/audio_socket"
+        )
+    }
+
     getVideoWebsocketEndpoint() {
     //   return (
     //     "ws://129.105.44.121:8080/video_socket"
     //   );
     return (
-        this.getWSSProtocol() +
-        "//" +
-        // window.location.host.split(":")[0] +
-        window.location.host +
-        "/video_socket"
-      );
+      "wss://video.tiilt-blinc.com/video_socket"
+    );
+  }
+
+   _generateHeaders(headers,data) {
+    let h = {}
+    if(!(data instanceof FormData)){
+      h =  {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      };
     }
-  
-     _generateHeaders(headers,data) {
-      let h = {}
-      if(!(data instanceof FormData)){
-        h =  {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        };
-      }
       
         let key = '';
         let val ='';
@@ -98,99 +80,97 @@ export class ApiService {
       }
       return h;
     }
-  
-    get(apipath, headers){
-      return fetch(this.getEndpoint() + apipath, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
-        headers: this._generateHeaders(headers),
-        redirect: 'follow', // manual, *follow, error
-        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      })
+
+    get(apipath, headers) {
+        return fetch(this.getEndpoint() + apipath, {
+            method: "GET", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "include", // include, *same-origin, omit
+            headers: this._generateHeaders(headers),
+            redirect: "follow", // manual, *follow, error
+            //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        })
     }
-  
-    delete(apipath, headers){
-      return fetch(this.getEndpoint() + apipath, {
-        method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
-        headers: this._generateHeaders(headers),
-        redirect: 'follow', // manual, *follow, error
-        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      })
+
+    delete(apipath, headers) {
+        return fetch(this.getEndpoint() + apipath, {
+            method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "include", // include, *same-origin, omit
+            headers: this._generateHeaders(headers),
+            redirect: "follow", // manual, *follow, error
+            //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        })
     }
-  
-    post(apipath, data,headers){
-     return  fetch(this.getEndpoint() + apipath, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
-        headers: this._generateHeaders(headers,data),
-        redirect: 'follow', // manual, *follow, error
-        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-      })
+
+    post(apipath, data, headers) {
+        return fetch(this.getEndpoint() + apipath, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "include", // include, *same-origin, omit
+            headers: this._generateHeaders(headers, data),
+            redirect: "follow", // manual, *follow, error
+            //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        })
     }
-  
-    postFiles(apipath, data){
-      return fetch(this.getEndpoint() + apipath, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
-        headers: this._generateHeaders({},data),
-        redirect: 'follow', // manual, *follow, error
-        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: data // body data type must match "Content-Type" header
-      })
+
+    postFiles(apipath, data) {
+        return fetch(this.getEndpoint() + apipath, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "include", // include, *same-origin, omit
+            headers: this._generateHeaders({}, data),
+            redirect: "follow", // manual, *follow, error
+            //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: data, // body data type must match "Content-Type" header
+        })
     }
-  
-    put(apipath, data,headers){
-      return fetch(this.getEndpoint() + apipath, {
-        method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
-        headers: this._generateHeaders(headers,data),
-        redirect: 'follow', // manual, *follow, error
-        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-      })
+
+    put(apipath, data, headers) {
+        return fetch(this.getEndpoint() + apipath, {
+            method: "PUT", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "include", // include, *same-origin, omit
+            headers: this._generateHeaders(headers, data),
+            redirect: "follow", // manual, *follow, error
+            //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        })
     }
-  
+
     httpRequestCall(apipath, type, data) {
-      // Default options are marked with *
-      let fetchcall = undefined;
-        if(type === 'POST'){
-          fetchcall = this.post(apipath,data,{})
-        }else if(type === 'PUT'){
-          fetchcall = this.put(apipath,data,{})
-        }else if (type === 'GET'){
-          fetchcall = this.get(apipath,{})
-        }else if (type === 'DELETE'){
-          fetchcall = this.delete(apipath,{})
+        // Default options are marked with *
+        let fetchcall = undefined
+        if (type === "POST") {
+            fetchcall = this.post(apipath, data, {})
+        } else if (type === "PUT") {
+            fetchcall = this.put(apipath, data, {})
+        } else if (type === "GET") {
+            fetchcall = this.get(apipath, {})
+        } else if (type === "DELETE") {
+            fetchcall = this.delete(apipath, {})
         }
-        return fetchcall;
+        return fetchcall
     }
-  
-    httpRequestCallWithHeader(apipath, type, data,headers) {
-      // Default options are marked with *
-      let fetchcall = undefined;
-        if(type === 'POST'){
-          fetchcall = this.post(apipath,data,headers)
-        }else if(type === 'PUT'){
-          fetchcall = this.put(apipath,data,headers)
-        }else if (type === 'GET'){
-          fetchcall = this.get(apipath,headers)
-        }else if (type === 'DELETE'){
-          fetchcall = this.delete(apipath,headers)
+
+    httpRequestCallWithHeader(apipath, type, data, headers) {
+        // Default options are marked with *
+        let fetchcall = undefined
+        if (type === "POST") {
+            fetchcall = this.post(apipath, data, headers)
+        } else if (type === "PUT") {
+            fetchcall = this.put(apipath, data, headers)
+        } else if (type === "GET") {
+            fetchcall = this.get(apipath, headers)
+        } else if (type === "DELETE") {
+            fetchcall = this.delete(apipath, headers)
         }
-        return fetchcall;
+        return fetchcall
     }
-  
-  }
-  
+}
