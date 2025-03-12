482dabc30b8bd31dbe2b46007f76a5894f9b2418
khalil8500 <henrylee10042004@gmail.com>
2025-03-12 20:11:51 +0000

    Commit general vs detailed view

--- frontend/src/components/section-box/section-box-component.js
+++ frontend/src/components/section-box/section-box-component.js
@@ -1,21 +1,28 @@
-
-import React from 'react'
-import style from './section-box.module.css'
+import React, { useState } from 'react';
+import style from './section-box.module.css';
 
 function AppSectionBoxComponent(props) {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+    console.log("Toggled! isExpanded:", !isExpanded);  // Debugging output
+  };
 
   return (
     <>
-      <div className={style.heading}>
+      <div className={style.heading} onClick={toggleExpand} style={{ cursor: 'pointer' }}>
         {props.heading}
       </div>
-      <div className={style["section-container"]} style={props.maxHeight!==undefined ? { maxHeight: props.maxHeight + 'px' } : {}}>
-        <React.Fragment>
-          {props.children}
-        </React.Fragment>
-      </div>
+      {isExpanded && (
+        <div className={style["section-container"]} style={props.maxHeight !== undefined ? { maxHeight: props.maxHeight + 'px' } : {}}>
+          <React.Fragment>
+            {props.children}
+          </React.Fragment>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export {AppSectionBoxComponent}
+export { AppSectionBoxComponent };
