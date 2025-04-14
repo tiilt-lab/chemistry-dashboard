23504d5c9aedf9ba216e16256a511ae39a3968a9
khalil8500 <khalil8500@gmail.com>
2025-04-14 14:50:47 -0500

    removed redundant components and reorganized individual metrics component into componets

--- frontend/src/components/infographics-view/infographics-view-component.js
+++ frontend/src/components/infographics-view/infographics-view-component.js
@@ -6,7 +6,7 @@ import { AppRadarComponent } from "radar/radar-component";
 import { AppKeywordsComponent } from "keywords/keywords-component";
 import style from "byod-join/byod-join.module.css";
 import { adjDim, isLargeScreen } from "myhooks/custom-hooks";
-import {AppIndividualFeaturesComponent} from "individualmetrics/features-component";
+import { AppIndividualFeaturesComponent } from "components/individualmetrics/features-component";
 import React, { useState } from "react";
 import ReactSlider from "react-slider";
 
@@ -18,30 +18,26 @@ function AppInfographicsView(props) {
     <>
       {props.speakers && (
         <div
-        className="pod_overview-container-large__+DaY+"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          alignItems: "flex-start",
-          gap: "10px",
-          width: "auto",
-          maxWidth: "100%",
-          maxHeight: "calc(100vh - 100px)", // Ensures content doesn't exceed viewport
-          overflowY: "auto", // Enables internal scrolling
-        }}
+          className="pod_overview-container-large__+DaY+"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            alignItems: "flex-start",
+            gap: "10px",
+            width: "auto",
+            maxWidth: "100%",
+            maxHeight: "calc(100vh - 100px)", // Ensures content doesn't exceed viewport
+            overflowY: "auto", // Enables internal scrolling
+          }}
         >
-      
           <div className={style["comparison-column"]}>
-            
             <select
               id="speaker1"
               className={style["dropdown-input"]}
               style={{ width: adjDim(350) + "px" }}
               value={selectedSpkrId1}
-              onChange={(e) =>
-                setSelectedSpkrId1(parseInt(e.target.value, 10))
-              }
+              onChange={(e) => setSelectedSpkrId1(parseInt(e.target.value, 10))}
             >
               <option value="-1">Group</option>
               {props.speakers.map((speaker) => (
@@ -54,80 +50,92 @@ function AppInfographicsView(props) {
             {selectedSpkrId1 === -1 ? (
               // Content for All Speakers
               <>
-                {props.showBoxes.length > 0 && props.showBoxes[3]["clicked"] && (
-                  <AppSectionBoxComponent heading={"Expression and Thinking Style"}>
-                    <AppFeaturesComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )}
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[3]["clicked"] && (
+                    <AppSectionBoxComponent
+                      heading={"Expression and Thinking Style"}
+                    >
+                      <AppFeaturesComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
 
-                {props.showBoxes.length > 0 && props.showBoxes[4]["clicked"] && (
-                  <AppSectionBoxComponent heading={"Radar chart"}>
-                    <AppRadarComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts}
-                      radarTrigger={props.radarTrigger}
-                      start={props.startTime}
-                      end={props.endTime}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )}
-                
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[4]["clicked"] && (
+                    <AppSectionBoxComponent heading={"Radar chart"}>
+                      <AppRadarComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts}
+                        radarTrigger={props.radarTrigger}
+                        start={props.startTime}
+                        end={props.endTime}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
               </>
             ) : (
               // Content for specific speaker
               <>
-                 /* {props.showBoxes.length > 0 && props.showBoxes[3]["clicked"] && (
-                  <AppSectionBoxComponent heading={`Expression and Thinking Style`}>
-                    <AppFeaturesComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts.filter(t => t.speaker_id === selectedSpkrId1)}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )} 
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[3]["clicked"] && (
+                    <AppSectionBoxComponent
+                      heading={`Expression and Thinking Style`}
+                    >
+                      <AppFeaturesComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts.filter(
+                          (t) => t.speaker_id === selectedSpkrId1
+                        )}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
 
-                {props.showBoxes.length > 0 && props.showBoxes[4]["clicked"] && (
-                  <AppSectionBoxComponent heading={`Radar chart`}>
-                    <AppRadarComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts.filter(t => t.speaker_id === selectedSpkrId1)}
-                      radarTrigger={props.radarTrigger}
-                      start={props.startTime}
-                      end={props.endTime}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )}*/
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[4]["clicked"] && (
+                    <AppSectionBoxComponent heading={`Radar chart`}>
+                      <AppRadarComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts.filter(
+                          (t) => t.speaker_id === selectedSpkrId1
+                        )}
+                        radarTrigger={props.radarTrigger}
+                        start={props.startTime}
+                        end={props.endTime}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
 
-                {props.showBoxes.length > 0 && props.showBoxes[5]["clicked"] && (
-                  <AppSectionBoxComponent heading={`Participation and Impact Style`}>
-                    <AppIndividualFeaturesComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts.filter(t => t.speaker_id === selectedSpkrId1)}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )}
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[5]["clicked"] && (
+                    <AppSectionBoxComponent
+                      heading={`Participation and Impact Style`}
+                    >
+                      <AppIndividualFeaturesComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts.filter(
+                          (t) => t.speaker_id === selectedSpkrId1
+                        )}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
               </>
             )}
-
           </div>
 
           <div className={style["comparison-column"]}>
-            
             <select
               id="speaker2"
               className={style["dropdown-input"]}
               style={{ width: adjDim(350) + "px" }}
               value={selectedSpkrId2}
-              onChange={(e) =>
-                setSelectedSpkrId2(parseInt(e.target.value, 10))
-              }
+              onChange={(e) => setSelectedSpkrId2(parseInt(e.target.value, 10))}
             >
               <option value="-1">Group</option>
               {props.speakers.map((speaker) => (
@@ -137,69 +145,82 @@ function AppInfographicsView(props) {
               ))}
             </select>
 
-            
-
             {selectedSpkrId2 === -1 ? (
               // Content for All Speakers
               <>
-                {props.showBoxes.length > 0 && props.showBoxes[3]["clicked"] && (
-                  <AppSectionBoxComponent heading={"Expression and Thinking Style"}>
-                    <AppFeaturesComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )}
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[3]["clicked"] && (
+                    <AppSectionBoxComponent
+                      heading={"Expression and Thinking Style"}
+                    >
+                      <AppFeaturesComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
 
-                {props.showBoxes.length > 0 && props.showBoxes[4]["clicked"] && (
-                  <AppSectionBoxComponent heading={"Radar chart"}>
-                    <AppRadarComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts}
-                      radarTrigger={props.radarTrigger}
-                      start={props.startTime}
-                      end={props.endTime}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )}
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[4]["clicked"] && (
+                    <AppSectionBoxComponent heading={"Radar chart"}>
+                      <AppRadarComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts}
+                        radarTrigger={props.radarTrigger}
+                        start={props.startTime}
+                        end={props.endTime}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
               </>
             ) : (
               // Content for specific speaker
               <>
-                 /* {props.showBoxes.length > 0 && props.showBoxes[3]["clicked"] && (
-                  <AppSectionBoxComponent heading={`Expression and Thinking Style`}>
-                    <AppFeaturesComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts.filter(t => t.speaker_id === selectedSpkrId2)}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )} 
-
-                {props.showBoxes.length > 0 && props.showBoxes[4]["clicked"] && (
-                  <AppSectionBoxComponent heading={`Radar chart`}>
-                    <AppRadarComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts.filter(t => t.speaker_id === selectedSpkrId2)}
-                      radarTrigger={props.radarTrigger}
-                      start={props.startTime}
-                      end={props.endTime}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )}*/
-
-                {props.showBoxes.length > 0 && props.showBoxes[5]["clicked"] && (
-                  <AppSectionBoxComponent heading={`Participation and Impact Style`}>
-                    <AppIndividualFeaturesComponent
-                      session={props.session}
-                      transcripts={props.displayTranscripts.filter(t => t.speaker_id === selectedSpkrId2)}
-                      showFeatures={props.showFeatures}
-                    />
-                  </AppSectionBoxComponent>
-                )}
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[3]["clicked"] && (
+                    <AppSectionBoxComponent
+                      heading={`Expression and Thinking Style`}
+                    >
+                      <AppFeaturesComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts.filter(
+                          (t) => t.speaker_id === selectedSpkrId2
+                        )}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[4]["clicked"] && (
+                    <AppSectionBoxComponent heading={`Radar chart`}>
+                      <AppRadarComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts.filter(
+                          (t) => t.speaker_id === selectedSpkrId2
+                        )}
+                        radarTrigger={props.radarTrigger}
+                        start={props.startTime}
+                        end={props.endTime}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
+                {props.showBoxes.length > 0 &&
+                  props.showBoxes[5]["clicked"] && (
+                    <AppSectionBoxComponent
+                      heading={`Participation and Impact Style`}
+                    >
+                      <AppIndividualFeaturesComponent
+                        session={props.session}
+                        transcripts={props.displayTranscripts.filter(
+                          (t) => t.speaker_id === selectedSpkrId2
+                        )}
+                        showFeatures={props.showFeatures}
+                      />
+                    </AppSectionBoxComponent>
+                  )}
               </>
             )}
           </div>
