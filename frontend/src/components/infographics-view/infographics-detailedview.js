470bdaae4bcbb39fba54ce597ce64258c8c5727e
khalil8500 <henrylee10042004@gmail.com>
2025-04-15 23:50:57 +0000

    fixed timeline slider

--- frontend/src/components/infographics-view/infographics-detailedview.js
+++ frontend/src/components/infographics-view/infographics-detailedview.js
@@ -2,12 +2,11 @@ import { AppSectionBoxComponent } from "components/section-box/section-box-compo
 import { AppTimelineSlider } from "components/timeline-slider/timeline-slider-component";
 import { AppTimeline } from "timeline/timeline-component";
 import { AppFeaturesComponent } from "features/features-component";
-// import { AppIndividualFeaturesComponent } from "individualmetrics/features-component";
+//import { AppIndividualFeaturesComponent } from "frontend\src\individualmetrics\features-component.js"
 import { AppRadarComponent } from "radar/radar-component";
 import { AppKeywordsComponent } from "keywords/keywords-component";
 import style from "byod-join/byod-join.module.css";
 import { adjDim, isLargeScreen } from "myhooks/custom-hooks";
-
 import React, { useState } from "react";
 import ReactSlider from "react-slider";
 
@@ -15,179 +14,187 @@ function AppInfographicsDetailedView(props) {
   const [selectedSpkrId1, setSelectedSpkrId1] = useState(-1);
   const [selectedSpkrId2, setSelectedSpkrId2] = useState(-1);
 
+const transcripts1 = selectedSpkrId1 === -1
+  ? props.displayTranscripts || []
+  : (props.displayTranscripts || []).filter(t => t.speaker_id === selectedSpkrId1);
+
+const transcripts2 = selectedSpkrId2 === -1
+  ? props.displayTranscripts || []
+  : (props.displayTranscripts || []).filter(t => t.speaker_id === selectedSpkrId2);
+
   return (
     <>
       {props.speakers && (
-        <div
-        className="pod_overview-container-large__+DaY+"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          alignItems: "flex-start",
-          gap: "10px",
-          width: "auto",
-          maxWidth: "100%",
-          maxHeight: "calc(100vh - 100px)", // Ensures content doesn't exceed viewport
-          overflowY: "auto", // Enables internal scrolling
-        }}
+        
+        <><>
+          {props.showBoxes.length > 0 && props.showBoxes[0]["clicked"] && (
+            <AppSectionBoxComponent heading={"Timeline control"}>
+              <AppTimelineSlider
+                id="timeSlider"
+                inputChanged={props.setRange} />
+            </AppSectionBoxComponent>
+          )}
+        </><div
+          className="pod_overview-container-large__+DaY+"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            alignItems: "flex-start",
+            gap: "10px",
+            width: "auto",
+            maxWidth: "100%",
+            maxHeight: "calc(100vh - 100px)", // Ensures content doesn't exceed viewport
+            overflowY: "auto", // Enables internal scrolling
+          }}
         >
-      
-          <div className={style["comparison-column"]}>
-            
-            <select
-              id="speaker1"
-              className={style["dropdown-input"]}
-              style={{ width: adjDim(350) + "px" }}
-              value={selectedSpkrId1}
-              onChange={(e) =>
-                setSelectedSpkrId1(parseInt(e.target.value, 10))
-              }
-            >
-              <option value="-1">Group</option>
-              {props.speakers.map((speaker) => (
-                <option key={speaker["id"]} value={speaker["id"]}>
-                  {speaker["alias"]}
-                </option>
-              ))}
-            </select>
 
-            {props.showBoxes.length > 0 && props.showBoxes[0]["clicked"] && (
-              <AppSectionBoxComponent heading={"Timeline control"}>
-                <AppTimelineSlider
-                  id="timeSlider1"
-                  inputChanged={props.setRange}
-                />
-              </AppSectionBoxComponent>
-            )}
 
-            {props.showBoxes.length > 0 && props.showBoxes[1]["clicked"] && (
-              <AppSectionBoxComponent heading={"Discussion timeline"}>
-                <AppTimeline
-                  clickedTimeline={props.onClickedTimeline}
-                  session={props.session}
-                  transcripts={props.displayTranscripts}
-                  start={props.startTime}
-                  end={props.endTime}
-                />
-              </AppSectionBoxComponent>
-            )}
 
-            {props.showBoxes.length > 0 && props.showBoxes[2]["clicked"] && (
-              <AppSectionBoxComponent heading={"Keyword detection"}>
-                <AppKeywordsComponent
-                  session={props.session}
-                  sessionDevice={props.sessionDevice}
-                  transcripts={props.displayTranscripts}
-                  start={props.startTime}
-                  end={props.endTime}
-                  fromclient={props.fromclient}
-                />
-              </AppSectionBoxComponent>
-            )}
+            <div className={style["comparison-column"]}>
 
-            {props.showBoxes.length > 0 && props.showBoxes[3]["clicked"] && (
-              <AppSectionBoxComponent heading={"Discussion features"}>
-                <AppFeaturesComponent
-                  session={props.session}
-                  transcripts={props.displayTranscripts}
-                  showFeatures={props.showFeatures}
-                />
-              </AppSectionBoxComponent>
-            )}
+              <select
+                id="speaker1"
+                className={style["dropdown-input"]}
+                style={{ width: adjDim(350) + "px" }}
+                value={selectedSpkrId1}
+                onChange={(e) => setSelectedSpkrId1(parseInt(e.target.value, 10))}
+              >
+                <option value="-1">Group</option>
+                {props.speakers.map((speaker) => (
+                  <option key={speaker["id"]} value={speaker["id"]}>
+                    {speaker["alias"]}
+                  </option>
+                ))}
+              </select>
 
-            {props.showBoxes.length > 0 && props.showBoxes[4]["clicked"] && (
-              <AppSectionBoxComponent heading={"Radar chart"}>
-                <AppRadarComponent
-                  session={props.session}
-                  transcripts={props.displayTranscripts}
-                  radarTrigger={props.radarTrigger}
-                  start={props.startTime}
-                  end={props.endTime}
-                  showFeatures={props.showFeatures}
-                />
-              </AppSectionBoxComponent>
-            )}
+              {/* 
+              // {props.showBoxes.length > 0 && props.showBoxes[0]["clicked"] && (
+              //  <AppSectionBoxComponent heading={"Timeline control"}>
+              //    <AppTimelineSlider
+              //      id="timeSlider1"
+              //      inputChanged={props.setRange} />
+              //  </AppSectionBoxComponent>
+              //)}
+              */}
 
-          </div>
+              {props.showBoxes.length > 0 && props.showBoxes[1]["clicked"] && (
+                <AppSectionBoxComponent heading={"Discussion timeline"}>
+                  <AppTimeline
+                    clickedTimeline={props.onClickedTimeline}
+                    session={props.session}
+                    transcripts={transcripts1}
+                    start={props.startTime}
+                    end={props.endTime} />
+                </AppSectionBoxComponent>
+              )}
 
-          <div className={style["comparison-column"]}>
-            
-            <select
-              id="speaker2"
-              className={style["dropdown-input"]}
-              style={{ width: adjDim(350) + "px" }}
-              value={selectedSpkrId2}
-              onChange={(e) =>
-                setSelectedSpkrId2(parseInt(e.target.value, 10))
-              }
-            >
-              <option value="-1">Group</option>
-              {props.speakers.map((speaker) => (
-                <option key={speaker["id"]} value={speaker["id"]}>
-                  {speaker["alias"]}
-                </option>
-              ))}
-            </select>
+              {props.showBoxes.length > 0 && props.showBoxes[2]["clicked"] && (
+                <AppSectionBoxComponent heading={"Keyword detection"}>
+                  <AppKeywordsComponent
+                    session={props.session}
+                    sessionDevice={props.sessionDevice}
+                    transcripts={transcripts1}
+                    start={props.startTime}
+                    end={props.endTime}
+                    fromclient={props.fromclient} />
+                </AppSectionBoxComponent>
+              )}
 
-            {props.showBoxes.length > 0 && props.showBoxes[0]["clicked"] && (
-              <AppSectionBoxComponent heading={"Timeline control"}>
-                <AppTimelineSlider
-                  id="timeSlider2"
-                  inputChanged={props.setRange}
-                />
-              </AppSectionBoxComponent>
-            )}
+              {props.showBoxes.length > 0 && props.showBoxes[3]["clicked"] && (
+                <AppSectionBoxComponent heading={"Discussion features"}>
+                  <AppFeaturesComponent
+                    session={props.session}
+                    transcripts={transcripts1}
+                    showFeatures={props.showFeatures} />
+                </AppSectionBoxComponent>
+              )}
 
-            {props.showBoxes.length > 0 && props.showBoxes[1]["clicked"] && (
-              <AppSectionBoxComponent heading={"Discussion timeline"}>
-                <AppTimeline
-                  clickedTimeline={props.onClickedTimeline}
-                  session={props.session}
-                  transcripts={props.displayTranscripts}
-                  start={props.startTime}
-                  end={props.endTime}
-                />
-              </AppSectionBoxComponent>
-            )}
+              {props.showBoxes.length > 0 && props.showBoxes[4]["clicked"] && (
+                <AppSectionBoxComponent heading={"Radar chart"}>
+                  <AppRadarComponent
+                    session={props.session}
+                    transcripts={transcripts1}
+                    radarTrigger={props.radarTrigger}
+                    start={props.startTime}
+                    end={props.endTime}
+                    showFeatures={props.showFeatures} />
+                </AppSectionBoxComponent>
+              )}
 
-            {props.showBoxes.length > 0 && props.showBoxes[2]["clicked"] && (
-              <AppSectionBoxComponent heading={"Keyword detection"}>
-                <AppKeywordsComponent
-                  session={props.session}
-                  sessionDevice={props.sessionDevice}
-                  transcripts={props.displayTranscripts}
-                  start={props.startTime}
-                  end={props.endTime}
-                  fromclient={props.fromclient}
-                />
-              </AppSectionBoxComponent>
-            )}
+            </div>
 
-            {props.showBoxes.length > 0 && props.showBoxes[3]["clicked"] && (
-              <AppSectionBoxComponent heading={"Discussion features"}>
-                <AppFeaturesComponent
-                  session={props.session}
-                  transcripts={props.displayTranscripts}
-                  showFeatures={props.showFeatures}
-                />
-              </AppSectionBoxComponent>
-            )}
+            <div className={style["comparison-column"]}>
 
-            {props.showBoxes.length > 0 && props.showBoxes[4]["clicked"] && (
-              <AppSectionBoxComponent heading={"Radar chart"}>
-                <AppRadarComponent
-                  session={props.session}
-                  transcripts={props.displayTranscripts}
-                  radarTrigger={props.radarTrigger}
-                  start={props.startTime}
-                  end={props.endTime}
-                  showFeatures={props.showFeatures}
-                />
-              </AppSectionBoxComponent>
-            )}
-          </div>
-        </div>
+              <select
+                id="speaker2"
+                className={style["dropdown-input"]}
+                style={{ width: adjDim(350) + "px" }}
+                value={selectedSpkrId2}
+                onChange={(e) => setSelectedSpkrId2(parseInt(e.target.value, 10))}
+              >
+                <option value="-1">Group</option>
+                {props.speakers.map((speaker) => (
+                  <option key={speaker["id"]} value={speaker["id"]}>
+                    {speaker["alias"]}
+                  </option>
+                ))}
+              </select>
+
+              {/*{props.showBoxes.length > 0 && props.showBoxes[0]["clicked"] && (
+                <AppSectionBoxComponent heading={"Timeline control"}>
+                  <AppTimelineSlider
+                    id="timeSlider2"
+                    inputChanged={props.setRange} />
+                </AppSectionBoxComponent>
+              )}
+                */}
+
+              {props.showBoxes.length > 0 && props.showBoxes[1]["clicked"] && (
+                <AppSectionBoxComponent heading={"Discussion timeline"}>
+                  <AppTimeline
+                    clickedTimeline={props.onClickedTimeline}
+                    session={props.session}
+                    transcripts={transcripts2}
+                    start={props.startTime}
+                    end={props.endTime} />
+                </AppSectionBoxComponent>
+              )}
+
+              {props.showBoxes.length > 0 && props.showBoxes[2]["clicked"] && (
+                <AppSectionBoxComponent heading={"Keyword detection"}>
+                  <AppKeywordsComponent
+                    session={props.session}
+                    sessionDevice={props.sessionDevice}
+                    transcripts={transcripts2}
+                    start={props.startTime}
+                    end={props.endTime}
+                    fromclient={props.fromclient} />
+                </AppSectionBoxComponent>
+              )}
+
+              {props.showBoxes.length > 0 && props.showBoxes[3]["clicked"] && (
+                <AppSectionBoxComponent heading={"Discussion features"}>
+                  <AppFeaturesComponent
+                    session={props.session}
+                    transcripts={transcripts2}
+                    showFeatures={props.showFeatures} />
+                </AppSectionBoxComponent>
+              )}
+
+              {props.showBoxes.length > 0 && props.showBoxes[4]["clicked"] && (
+                <AppSectionBoxComponent heading={"Radar chart"}>
+                  <AppRadarComponent
+                    session={props.session}
+                    transcripts={transcripts2}
+                    radarTrigger={props.radarTrigger}
+                    start={props.startTime}
+                    end={props.endTime}
+                    showFeatures={props.showFeatures} />
+                </AppSectionBoxComponent>
+              )}
+            </div>
+          </div></>
       )}
     </>
   );
