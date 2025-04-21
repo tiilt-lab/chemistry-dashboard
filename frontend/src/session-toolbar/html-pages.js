e9e16678ce52f5366a1c56a0d4927e41561a6856
khalil8500 <khalil8500@gmail.com>
2025-04-21 13:27:45 -0500

    change session toolbar from footer to side bar to be able to hold more buttons

--- frontend/src/session-toolbar/html-pages.js
+++ frontend/src/session-toolbar/html-pages.js
@@ -1,42 +1,51 @@
-import style from './session-toolbar.module.css'
-import style2 from '../dialog/dialog.module.css'
-import {GenericDialogBox} from '../dialog/dialog-component'
-import {AppSpinner} from '../spinner/spinner-component'
-import React from 'react'
-import { adjDim } from '../myhooks/custom-hooks'
+import style from "./session-toolbar.module.css";
+import style2 from "../dialog/dialog.module.css";
+import { GenericDialogBox } from "../dialog/dialog-component";
+import { AppSpinner } from "../spinner/spinner-component";
+import React from "react";
+import { adjDim } from "../myhooks/custom-hooks";
+import { isLargeScreen } from "../myhooks/custom-hooks";
 
-function AppSessionPage(props){
+function AppSessionPage(props) {
+  return (
+    <>
+      <div
+        className={
+          props.sessionEnding
+            ? `${style["session-toolbar"]} ${style["dialog-blur"]}`
+            : style["session-toolbar"]
+        }
+      >
+        <span className={style["session-time"]}>
+          <div className={style.time}>{props.timeText}</div>
+          <div className={style.info}>Total duration</div>
+        </span>
+        {props.menus.map((menu, index) => (
+          <button className={
+            isLargeScreen()
+              ? `${style["toolbar-button"]} ${style["basic-button"]} ${style["medium-button"]}`
+              : `${style["toolbar-button"]} ${style["basic-button"]} ${style["small-button"]}`}
+           >{`${index + 1}. ${menu.title}`}</button>
+        ))}
+        {props.session.recording ? (
+          <span className={style["session-end"]} onClick={props.onEndSession}>
+            <button className={style["end-button"]}>End</button>
+          </span>
+        ) : (
+          <></>
+        )}
+      </div>
 
-    return(
-        <>
-        <div className={props.sessionEnding ? `${style["footer-bar"]} ${style["dialog-blur"]}`: style["footer-bar"]} >
-            <div className={style["session-toolbar"]} style={{"grid-template-columns": adjDim(120) + "px auto " + adjDim(120) + "px",}}>
-                <span className={style["session-time"]}>
-                    <div className={style.time}>{props.timeText}</div>
-                    <div className={style.info}>Total duration</div>
-                </span>
-                <span className={style["middle-content"]}>
-                    <React.Fragment>
-                        {props.innerhtml}
-                    </React.Fragment>
-                </span>
-                {props.session.recording ?
-                <span  className={style["session-end"]} onClick={props.onEndSession}>
-                    <button className={style["end-button"]}>End</button>
-                </span>
-                :
-                <></>}
-            </div>
+      <GenericDialogBox show={props.sessionEnding}>
+        <div className={style["dialog-content"]}>
+          <div className={style2["dialog-heading"]}>Session Ending...</div>
+          <div className={style["dialog-spinner"]}>
+            <AppSpinner></AppSpinner>
+          </div>
         </div>
-
-<GenericDialogBox show={props.sessionEnding} >
-    <div className={style["dialog-content"]}>
-        <div className={style2["dialog-heading"]}>Session Ending...</div>
-        <div className={style["dialog-spinner"]}><AppSpinner></AppSpinner></div>
-    </div>
-</GenericDialogBox>
-        </>
-    )
+      </GenericDialogBox>
+    </>
+  );
 }
 
-export {AppSessionPage}
+export { AppSessionPage };
