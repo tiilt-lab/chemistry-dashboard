03f80ecc0f6408331b188135a3efdbabe7439182
khalil8500 <khalil8500@gmail.com>
2025-04-29 06:41:57 +0000

    More changes to add tailwind in poddetails. Edited section box

--- frontend/src/components/section-box/section-box-component.jsx
+++ frontend/src/components/section-box/section-box-component.jsx
@@ -1,28 +1,37 @@
-import React, { useState } from 'react';
-import style from './section-box.module.css';
+import React, { useState } from "react"
+import style from "./section-box.module.css"
 
 function AppSectionBoxComponent(props) {
-  const [isExpanded, setIsExpanded] = useState(true);
+    const [isExpanded, setIsExpanded] = useState(true)
 
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-    console.log("Toggled! isExpanded:", !isExpanded);  // Debugging output
-  };
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded)
+        console.log("Toggled! isExpanded:", !isExpanded) // Debugging output
+    }
 
-  return (
-    <>
-      <div className={style.heading} onClick={toggleExpand} style={{ cursor: 'pointer' }}>
-        {props.heading}
-      </div>
-      {isExpanded && (
-        <div className={style["section-container"]} style={props.maxHeight !== undefined ? { maxHeight: props.maxHeight + 'px' } : {}}>
-          <React.Fragment>
-            {props.children}
-          </React.Fragment>
-        </div>
-      )}
-    </>
-  );
+    return (
+        <>
+            <div
+                className={
+                    "relative border-black bg-[#FCFDFF] text-center font-sans text-base/relaxed font-normal"
+                }
+                style={
+                    props.maxHeight !== undefined
+                        ? { maxHeight: props.maxHeight + "px" }
+                        : {}
+                }
+            >
+                <div
+                    className="my-2 rounded-sm border-b-black bg-[#375faf] px-0.5 font-sans text-lg/relaxed font-bold text-[#ffffff]"
+                    onClick={toggleExpand}
+                    style={{ cursor: "pointer" }}
+                >
+                    {props.heading}
+                </div>
+                <React.Fragment>{isExpanded && props.children}</React.Fragment>
+            </div>
+        </>
+    )
 }
 
-export { AppSectionBoxComponent };
+export { AppSectionBoxComponent }
