b65329c95145ced12d27931a85e78fe10f388808
khalil8500 <khalil8500@gmail.com>
2025-05-07 13:10:21 +0000

    Revert "Revert "Revert "Merge branch 'image_emotion_dedection' of https://github.com/tiilt-lab/chemistry-dashboard into image_emotion_dedection"""
    This reverts commit cf77f3f177a79c3514d5be209e5651e5179223dc.

--- video_processing/callbacks.py
+++ video_processing/callbacks.py
@@ -5,6 +5,21 @@ from datetime import datetime
 import logging
 import base64
 
+    
+def post_cartoonized_image(source, sessionId,deviceId, byteimage):
+    encoded = base64.b64encode(byteimage) 
+    result = {
+        'source': source,
+        'sessionid' : sessionId,
+        'deviceid' : deviceId,
+        'image': encoded
+    }
+    try:
+        response = requests.post(config.cartoonize_image_callback(),json=result)
+        return response.status_code == 200
+    except Exception as e:
+        logging.info('cartoonize image callback  failed: {0}'.format(e))
+        return False
 
 def get_redis_session_key(auth_key):
     payload = {
