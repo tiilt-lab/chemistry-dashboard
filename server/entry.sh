0a37dd005c9b08ee3d3109b7370866eaf1c19dd5
khalil8500 <khalil8500@gmail.com>
2025-06-05 14:11:51 -0500

    update entry and redis

--- server/entry.sh
+++ server/entry.sh
@@ -5,6 +5,6 @@ export FLASK_APP=discussion_capture.py
 flask db upgrade
 
 #create_user
-python create_user_from.py blinc@mudcat11 super
+python3 create_user_from.py blinc@mudcat11 super
 
 exec "$@"
