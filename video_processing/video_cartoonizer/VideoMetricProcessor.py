0baa5c5fdb26740fa55fd178643d629c8312099f
Tochukwu <domisky2003@yahoo.com>
2026-03-30 23:00:21 +0000

    pushing changes

--- video_processing/video_cartoonizer/VideoMetricProcessor.py
+++ video_processing/video_cartoonizer/VideoMetricProcessor.py
@@ -36,12 +36,10 @@ class VideoMetricAnalytics:
                         #if head gaze is focused on other object
                         pred_attention_level = self.AttentionTracking.track_person_level_of_attention(object_class_id,person_id)
                         pred_object_focused_on = object_class_name
-                        logging.info("For person_id: {0}, frame_index: {1}, predicted emotion is {2}, predicted attention level is {3} and predicted object focused on is {4}".format(person_id, frame_index, pred_emotion, pred_attention_level, pred_object_focused_on))
-                        frame_raw = cv2.circle(frame_raw, (gaze_x, gaze_y), int(frame_height/50.0), (255, 0, 0), 2) 
-                        cv2.arrowedLine(frame_raw,(int((h_bbox[0]+h_bbox[2])/2),int((h_bbox[1]+h_bbox[3])/2)),(gaze_x, gaze_y), (230,253,11),thickness=3)
-                        # plot_one_box([int(other_objects[object_index][3]),int(other_objects[object_index][4]),int(other_objects[object_index][5]),int(other_objects[object_index][6])], frame_raw, label=str(other_objects[object_index][1]), color=(0,0,0), line_thickness=3)
-                        # cv2.putText(frame_raw, str(other_objects[object_index][2]), (int(other_objects[object_index][5]), int(other_objects[object_index][6]) + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.9,(0,0,255) , 2)
-                        cv2.imwrite(os.path.join(self.base_path,'annotated_frames', str(frame_index)+'.jpg'), frame_raw) 
+                        # logging.info("For person_id: {0}, frame_index: {1}, predicted emotion is {2}, predicted attention level is {3} and predicted object focused on is {4}".format(person_id, frame_index, pred_emotion, pred_attention_level, pred_object_focused_on))
+                        # frame_raw = cv2.circle(frame_raw, (gaze_x, gaze_y), int(frame_height/50.0), (255, 0, 0), 2) 
+                        # cv2.arrowedLine(frame_raw,(int((h_bbox[0]+h_bbox[2])/2),int((h_bbox[1]+h_bbox[3])/2)),(gaze_x, gaze_y), (230,253,11),thickness=3)
+                        # cv2.imwrite(os.path.join(self.base_path,'annotated_frames', str(frame_index)+'.jpg'), frame_raw) 
                     else:
                         # print("no closer object: ")
                         #the gaze is not focused on any object detected 
